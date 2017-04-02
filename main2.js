1cfea2f2d94c136d7a1dc513f0b2db735197f7f0
Donald Tofuah <dtofuah@gmail.com>
2017-04-02 09:50:31 -0400

    initial commit

--- main2.js
+++ main2.js
@@ -1,37 +1,64 @@
 
 
-//Button Wars!//
+// Button Wars!//
 
-function button1(name){
-document.getElementById("print_text").innerHTML = name + ".";
+function button1(name) {
+	document.getElementById("first_button").innerHTML = name + ".";
 }
 
 function button2(name) {
-document.getElementById("print_text").innerHTML = name + ".";
+	document.getElementById("second_button").innerHTML = name + ".";
 }
 
 
 // Don't Hover!//
 
-	document.getElementById("dont_hover").addEventListener("mouseover", onClick);
-  function onClick(event){
-		alert("Hey, I told you not to hover over me!");
-	}
+document.getElementById("dont_hover").addEventListener("mouseover", onClick);
+function onClick(event) {
+	alert("Hey, I told you not to hover over me!");
+}
 
-// Events
+// Text appearing on screen
 function myFunction() {
-document.getElementById("header").innerHTML = String.fromCharCode(event.charCode);
+	document.getElementById("header").innerHTML = String.fromCharCode(event.keyCode);
 }
 
 
-// validatopn checks
-function logPassword(){
-var password = document.getElementById('password').value;
+// validation checks
+var formElement = document.getElementById('form');
+formElement.addEventListener("submit", function(event) {
+	var password = document.getElementById('pass').value;
 
-  if (password == "1234678"){
-    console.log("good pasword");
-  }
-    else{
-      alert("Error: Please check that you've entered password!");
-    }
-}
+
+	if (password === "12345678") {
+		alert("Correct Password");
+	}
+	else {
+		alert("Error: You have entered a wrong password!");
+	}
+});
+
+formElement.addEventListener("submit", function(event) {
+	var username = document.getElementById('user').value;
+
+
+	if (username === "dtofuah") {
+		alert("Correct Username");
+	}
+	else {
+		alert("Error: You have entered a wrong Username!");
+	}
+});
+
+
+formElement.addEventListener("submit", function(event) {
+	var Email = document.getElementById('email').value;
+
+
+	if (Email === "dtofuah@gmail.com") {
+		alert("Correct Email Address");
+	}
+	else {
+		alert("Error: You have entered a wrong Email Address!");
+	}
+});
